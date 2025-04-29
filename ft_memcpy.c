f0742131e3eecf724d884acd99316c1ed1e33454
Antonio Cossari <acossari@f0r2s1.codam.nl>
2025-04-29 20:17:16 +0200

    Added 3 new files about memory

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -11,3 +11,21 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	/* Usiamo un puntatore a unsigned char per trattare la memoria come un array di byte. */
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src; 
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
--- ft_memset.c
+++ ft_memset.c
@@ -11,8 +11,40 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t n)
 {
-	return ();
+	size_t			i;
+	unsigned char	*p;
+
+	// Cast del puntatore generico a puntatore ad unsigned char per assicurarci
+	// Per garantire che ogni assegnazione scriva esattamente un “byte” senza segni ecc
+	p = (unsigned char *)b;                 
+	i = 0;
+	while (i < n)                            // ripeti fino a n byte, non e una string ama un'area di memoria
+	{
+		p[i] = (unsigned char)c;             // Castiamo c a unsigned char per ottenere il valore tra 0 e 255.
+		i++;
+	}
+	return (b); // restituisce il puntatore originale a inizio buffer
+}
+
+int main(void)
+{
+	char buffer[16];
+
+	// Inizializziamo buffer con un pattern riconoscibile
+	for (int i = 0; i < 15; i++)
+		buffer[i] = '-';
+	buffer[15] = '\0'; // La 16esima posizione
+
+	printf("Prima:  \"%s\"\n", buffer);
+
+	// Riempie i primi 10 byte con 'A'
+	ft_memset(buffer, 'A', 10);
+
+	printf("Dopo:   \"%s\"\n", buffer);
+
+	return 0;
 }

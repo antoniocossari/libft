a18b4f58161f2f7abf43c6995ea12e7443ca20a0
antonio <antoniocossari.info@gmail.com>
2025-05-09 11:50:25 +0200

    fix(tests.h): fixed tests header

--- tests/tests.h
+++ tests/tests.h
@@ -6,7 +6,7 @@
 /*   By: acossari <acossari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 12:56:25 by acossari          #+#    #+#             */
-/*   Updated: 2025/05/08 14:42:01 by acossari         ###   ########.fr       */
+/*   Updated: 2025/05/09 11:49:14 by acossari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,71 @@
 # define TESTS_H
 
 # include <assert.h>
-# include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
+
+/*
+ * Undefine any existing declarations or macros for strlcat/strlcpy
+ * so that our static inline versions won’t conflict with the
+ * ones in <string.h> (glibc exports non-static prototypes).
+ */
+# undef strlcat
+# undef strlcpy
+
+/* Now include string.h to get strlen, memcpy, strchr, strncmp, etc. */
+# include <string.h>
 # include "libft.h"
 
-# undef strlcat
+/*
+ * Inline implementation of strlcat:
+ * Append src to the end of dst, for a total buffer size dstsize.
+ * Always NUL-terminates (if there’s room). Returns the initial
+ * length of dst plus the length of src.
+ *
+ * Official reference: OpenBSD strlcat(3) / FreeBSD strlcat(3)
+ */
 static inline size_t
 strlcat(char *dst, const char *src, size_t dstsize)
 {
-	char *odst = dst;
-	size_t n = dstsize;
-	size_t dlen;
+	char   *odst = dst;
+	size_t  n    = dstsize;
+	size_t  dlen;
 
+	/* Find end of dst and adjust bytes left but don’t go past dstsize */
 	while (n-- != 0 && *dst != '\0')
 		dst++;
 	dlen = dst - odst;
 	n = dstsize - dlen;
+
 	if (n == 0)
 		return (dlen + strlen(src));
+
+	/* Copy as many bytes as will fit, leaving room for NUL */
 	{
 		size_t copy_n = dstsize - dlen - 1;
-		size_t srclen = strlen(src);
-		if (copy_n > srclen)
-			copy_n = srclen;
+		size_t slen   = strlen(src);
+
+		if (copy_n > slen)
+			copy_n = slen;
 		memcpy(odst + dlen, src, copy_n);
 		odst[dlen + copy_n] = '\0';
 	}
+
 	return (dlen + strlen(src));
 }
 
-# undef strlcpy
+/*
+ * Inline implementation of strlcpy:
+ * Copy up to dstsize-1 bytes from src to dst, NUL-terminating dst.
+ * Returns the total length of src; if that’s >= dstsize, truncation occurred.
+ *
+ * Official reference: OpenBSD strlcpy(3) / FreeBSD strlcpy(3)
+ */
 static inline size_t
 strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t srclen = strlen(src);
+	size_t slen = strlen(src);
 	size_t i;
 
 	if (dstsize != 0)
@@ -58,24 +87,30 @@ strlcpy(char *dst, const char *src, size_t dstsize)
 			dst[i] = src[i];
 		dst[i] = '\0';
 	}
-	return (srclen);
+
+	return (slen);
 }
 
-# undef strnstr
+/*
+ * Inline strnstr:
+ * Locate the first occurrence of needle in haystack, where not more
+ * than len characters are searched. Characters after a ‘\0’ are not searched.
+ */
 static inline char *
 strnstr(const char *haystack, const char *needle, size_t len)
 {
-	const char *h;
-	size_t lneedle;
+	size_t nlen;
 
 	if (*needle == '\0')
 		return (char *)haystack;
-	lneedle = strlen(needle);
-	for (h = haystack; (h = strchr(h, *needle)) != NULL; h++) {
-		if ((size_t)(h - haystack) + lneedle > len)
-			return NULL;
-		if (strncmp(h, needle, lneedle) == 0)
-			return (char *)h;
+	nlen = strlen(needle);
+
+	for (; *haystack; haystack++)
+	{
+		if (haystack[0] == needle[0] &&
+			(size_t)(haystack - (const char *)haystack) + nlen <= len &&
+			strncmp(haystack, needle, nlen) == 0)
+			return (char *)haystack;
 	}
 	return NULL;
 }

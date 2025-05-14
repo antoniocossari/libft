a485e683ebe00c25dbe054ebe3e1eff7e0f98538
antonio <antoniocossari.info@gmail.com>
2025-05-14 20:18:10 +0200

    refactor(libft): improve ft_calloc overflow check and ft_strdup clarity
    - ft_calloc: use SIZE_MAX for overflow detection and include <limits.h> locally for readability
    - ft_strdup: rename  to  and switch to an index-based loop for clearer character copying

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,25 @@
 /*   By: acossari <acossari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 16:08:37 by acossari          #+#    #+#             */
-/*   Updated: 2025/05/05 19:57:53 by acossari         ###   ########.fr       */
+/*   Updated: 2025/05/14 19:46:49 by acossari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>	/* for SIZE_MAX */
 /*
- * ft_calloc:
- *   Allocate memory for an array of `nmemb` elements of `size` bytes each,
- *   initialize all bytes to zero, and return a pointer to the allocated block.
- *   Returns NULL if the allocation fails.
- */
+** ft_calloc: allocate and zero-initialize an array
+** @nmemb: number of elements to allocate
+** @size:  size in bytes of each element
+** Return: pointer to allocated memory,
+**         or NULL on overflow or allocation failure
+*/
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	total;
 	void	*ptr;
 
-	if (size != 0 && nmemb > ((size_t)-1) / size)
+	if (size != 0 && nmemb > SIZE_MAX / size)
 		return (NULL);
 	total = nmemb * size;
 	ptr = malloc(total);
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,33 +6,33 @@
 /*   By: acossari <acossari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 16:10:09 by acossari          #+#    #+#             */
-/*   Updated: 2025/05/05 19:58:08 by acossari         ###   ########.fr       */
+/*   Updated: 2025/05/14 20:17:07 by acossari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 /*
- * ft_strdup:
- *   Allocate memory and return a duplicate of the string `s1`.
- *   Memory is obtained with malloc and must be freed by the caller.
- *   Returns NULL if allocation fails.
- */
+** ft_strdup: duplicate a string by allocating new memory
+** @s1: the null-terminated string to copy
+** Return: pointer to the new string (must be freed),
+**         or NULL if allocation fails
+*/
 char	*ft_strdup(const char *s1)
 {
-	char	*copy;
+	char	*dup;
 	size_t	len;
 	size_t	i;
 
 	len = ft_strlen(s1);
-	copy = malloc(len + 1);
-	if (!copy)
+	dup = malloc(len + 1);
+	if (!dup)
 		return (NULL);
 	i = 0;
 	while (i < len)
 	{
-		copy[i] = s1[i];
+		dup[i] = s1[i];
 		i++;
 	}
-	copy[i] = '\0';
-	return (copy);
+	dup[i] = '\0';
+	return (dup);
 }
